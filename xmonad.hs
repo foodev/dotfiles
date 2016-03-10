4ca76811062c1adea98e1335f74a86c1474fc153
foodev <jonas@foodev.de>
2016-03-10 22:13:32 +0100

    Almost completly rewrite xmonad.hs and got rid of xmobar
    It's integrating into Xfce well now and it's working with multiple monitors with five workspaces on each.

--- xmonad.hs
+++ xmonad.hs
@@ -1,90 +1,146 @@
 import XMonad
 import XMonad.Config.Xfce
-import XMonad.Util.EZConfig(additionalKeys)
-import XMonad.Util.Run(spawnPipe, hPutStrLn)
-import XMonad.Util.WorkspaceCompare
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.EwmhDesktops
+
+import XMonad.Util.EZConfig (removeKeys, additionalKeys)
+import XMonad.Util.WorkspaceCompare (getSortByTag)
+import XMonad.Util.Scratchpad
+
+import XMonad.Hooks.ManageDocks (avoidStruts)
+import XMonad.Hooks.EwmhDesktops (fullscreenEventHook)
 import XMonad.Hooks.DynamicLog
-import XMonad.Layout.IM
-import XMonad.Layout.Grid
+import XMonad.Hooks.Place
+import XMonad.Hooks.InsertPosition
+import XMonad.Hooks.UrgencyHook
+
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.Spacing
-import XMonad.Layout.Gaps
 import XMonad.Layout.IndependentScreens
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Simplest
+import XMonad.Layout.Renamed
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.Fullscreen (fullscreenFull, fullscreenManageHook)
+
 import qualified Data.Map as M
 import qualified XMonad.StackSet as W
-import Data.Ratio ((%))
+import qualified DBus as D
+import qualified DBus.Client as D
+import qualified Codec.Binary.UTF8.String as UTF8
 
-xmobarBinary = "/usr/bin/xmobar"
-xmobarrc = "/home/jonas/.xmobarrc"
+colorBase03 = "#002b36"
+colorBase02 = "#073642"
+colorBase01 = "#586e75"
+colorBase00 = "#657b83"
+colorBase0 = "#839496"
+colorBase1 = "#93a1a1"
+colorBase2 = "#eee8d5"
+colorBase3 = "#fdf6e3"
+colorYellow = "#b58900"
+colorOrange = "#cb4b16"
+colorRed = "#dc322f"
+colorMagenta = "#d33682"
+colorViolet = "#6c71c4"
+colorBlue = "#268bd2"
+colorCyan = "#2aa198"
+colorGreen = "#859900"
 
-xmobarFontFace = "Fantasque Sans Mono" -- "xft:Source Code Pro:size=9"
-xmobarFontSize = 11
-
-normalColor = "#657b83"
-focusedColor = "#dc322f"
-activeColor = "#b58900"
-
-myLayout numberOfScreens = onWorkspaces screensDev layoutDev $ onWorkspaces screensChat layoutChat $ onWorkspaces screensGames layoutGames $ normal
+myLayout numberOfScreens = onWorkspaces screensGames layoutGames $ fullscreenFull $ normal
     where
-        screensDev   = withScreens numberOfScreens ["d"]
-        screensChat  = withScreens numberOfScreens ["c"]
-        screensGames = withScreens numberOfScreens ["g"]
+        screensGames = withScreens numberOfScreens ["V"]
 
-        normal       = spacing space $ tiled ||| Mirror tiled ||| Full
-        layoutDev    = spacing space $ Mirror tiledLarge ||| Full
-        layoutChat   = spacing space $ withIM (1%7) (Role "buddy_list") Grid ||| Full
+        normal       = renamed [CutWordsLeft 2] $ spacing 4 $ tiled ||| Mirror tiled ||| Mirror tiledLarge ||| Full
         layoutGames  = noBorders Simplest
 
-        tiled        = Tall 1 (3/100) (5/9)
+        tiled        = Tall 1 (3/100) (2/3)
         tiledLarge   = Tall 1 (3/100) (5/6)
-        space        = 4
+
+myManageHook = composeAll
+    [
+          className =? "Xfrun4" --> placeHook (fixed (0.5,0.5)) <+> doFloat
+        , fullscreenManageHook
+        , scratchpadManageHookDefault
+        , insertPosition Below Newer
+    ]
 
 myKeys conf@(XConfig {modMask = modm}) = M.fromList $
     [
         ((m .|. mod4Mask, k), windows $ onCurrentScreen f i)
-            | (i, k) <- zip (workspaces' conf) [xK_1 .. xK_9]
+            | (i, k) <- zip (workspaces' conf) [xK_1 .. xK_5]
             , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
     ]
 
-xmobarCommand (S s) = unwords [xmobarBinary, "-f", "\"xft:" ++ xmobarFontFace ++ ":size=" ++ show xmobarFontSize ++ "\"", "-B", "\"#002b36\"", "-F", "\"" ++ normalColor ++ "\"", "-t", "\" %StdinReader% }{ %date% \"", "-x", show s, xmobarrc ++ show s]
-
-pp h screen = marshallPP screen xmobarPP {
-                  ppOutput          = hPutStrLn h
-                , ppCurrent         = xmobarColor activeColor "" . wrap "[" "]"
-                , ppVisible         = xmobarColor activeColor ""
-                , ppHidden          = xmobarColor normalColor ""
-                , ppHiddenNoWindows = xmobarColor normalColor ""
-                , ppTitle           = xmobarColor activeColor ""
-                --, ppSort          = eetSortByIndex
-                }
-
 main = do
-    -- xmproc <- spawnPipe (xmobarBinary ++ xmobarrc)
     numberOfScreens <- countScreens
-    hs <- mapM (spawnPipe . xmobarCommand) [0..numberOfScreens-1]
+    dbus <- D.connectSession
+    getWellKnownName dbus
 
-    xmonad $ xfceConfig
-        {
-              modMask             = mod4Mask
-            , handleEventHook     = fullscreenEventHook
-            , terminal            = "xfce4-terminal"
-            , workspaces          = withScreens numberOfScreens ["w", "d", "c", "m", "v", "g"]
-            , borderWidth         = 2
-            , normalBorderColor   = normalColor
-            , focusedBorderColor  = focusedColor
-            , manageHook          = manageDocks <+> manageHook xfceConfig
-            , layoutHook          = avoidStruts $ myLayout numberOfScreens
-            , keys                = myKeys <+> keys xfceConfig
-            , logHook             = mapM_ dynamicLogWithPP $ zipWith pp hs [0..numberOfScreens]
+    xmonad $ withUrgencyHook NoUrgencyHook $ xfceConfig {
+            modMask             = mod4Mask
+          , terminal            = "xfce4-terminal"
+          , workspaces          = withScreens numberOfScreens ["I", "II", "III", "IV", "V"]
+          , borderWidth         = 3
+          , normalBorderColor   = colorBase0
+          , focusedBorderColor  = colorOrange
+          , handleEventHook     = fullscreenEventHook
+          , manageHook          = manageHook xfceConfig <+> myManageHook
+          , layoutHook          = avoidStruts $ myLayout numberOfScreens
+          , keys                = myKeys <+> keys xfceConfig
+          , logHook             = dynamicLogWithPP (prettyPrinter dbus)
         } `additionalKeys` [
-            ((mod4Mask, xK_p),
-                spawn "gmrun"),
-                -- spawn "dmenu_run -x 0 -y 0 -w 1920 -h 30 -fn 'Source Sans Code-9' -sb '#586e75' -sf '#dc322f' -s 0 -z"),
-            ((mod4Mask, xK_l),
-                spawn "light-locker-command -l")
+            ((mod4Mask, xK_asciicircum), spawn "xfce4-terminal")
+          , ((mod4Mask, xK_s), scratchpadSpawnActionTerminal "xfce4-terminal")
+          , ((mod4Mask, xK_p), spawn "xfrun4 --collapsed")
+          , ((controlMask .|. mod1Mask, xK_a), spawn "keepass --auto-type")
+          -- , ((mod4Mask, xK_l), spawn "light-locker-command -l")
+          , ((mod4Mask, xK_a), sendMessage MirrorShrink)
+          , ((mod4Mask, xK_z), sendMessage MirrorExpand)
+        ] `removeKeys` [
+            (mod4Mask .|. shiftMask, xK_Return)
         ]
 
+prettyPrinter :: D.Client -> PP
+prettyPrinter dbus = defaultPP
+    { ppOutput          = dbusOutput dbus
+    , ppTitle           = wrap "<b>" "</b>"
+    , ppCurrent         = pangoFgColor colorBase3 . pangoBgColor colorYellow . pangoSanitize . pad
+    , ppVisible         = pangoFgColor colorBase3 . pangoBgColor colorBase0 . pangoSanitize . pad
+    , ppHidden          = pangoFgColor colorBase3 . pangoBgColor colorBase0 . pangoSanitize . pad
+    , ppHiddenNoWindows = const ""
+    , ppUrgent          = pangoFgColor colorBase3 . pangoBgColor colorRed
+    , ppSep             = "  <b>·</b>  "
+    , ppSort            = getSortByTag
+    }
+
+getWellKnownName :: D.Client -> IO ()
+getWellKnownName dbus = do
+    D.requestName dbus (D.busName_ "org.xmonad.Log")
+                [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
+    return ()
+
+dbusOutput :: D.Client -> String -> IO ()
+dbusOutput dbus str = do
+    let signal = (D.signal (D.objectPath_ "/org/xmonad/Log") (D.interfaceName_ "org.xmonad.Log") (D.memberName_ "Update")) {
+                 D.signalBody = [D.toVariant (UTF8.decodeString str)]
+        }
+    D.emit dbus signal
+
+pangoFgColor :: String -> String -> String
+pangoFgColor fg = wrap left right
+    where
+        left  = "<span foreground=\"" ++ fg ++ "\">"
+        right = "</span>"
+
+pangoBgColor :: String -> String -> String
+pangoBgColor fg = wrap left right
+    where
+        left  = "<span background=\"" ++ fg ++ "\">"
+        right = "</span>"
+
+pangoSanitize :: String -> String
+pangoSanitize = foldr sanitize ""
+    where
+        sanitize '>'  xs = "&gt;" ++ xs
+        sanitize '<'  xs = "&lt;" ++ xs
+        sanitize '\"' xs = "&quot;" ++ xs
+        sanitize '&'  xs = "&amp;" ++ xs
+        sanitize x    xs = x:xs
